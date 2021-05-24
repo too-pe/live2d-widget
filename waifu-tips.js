ffa95fea53a19b23f378b17f1b364a6cdb2834ca
YuHang <74060144+too-pe@users.noreply.github.com>
2021-05-24 17:24:53 +0800

    Update waifu-tips.js

--- waifu-tips.js
+++ waifu-tips.js
@@ -91,7 +91,7 @@ function loadWidget(config) {
 		const devtools = () => {};
 		console.log("%c", devtools);
 		devtools.toString = () => {
-			showMessage("哈哈，你打开了控制台，是想要看看我的小秘密吗？", 6000, 9);
+			showMessage("哈哈哈哈哈哈哈哈哈哈，你打开了控制台，是想要看看我的小秘密吗？", 6000, 9);
 		};
 		window.addEventListener("copy", () => {
 			showMessage("你都复制了些什么呀，转载要记得加上出处哦！", 6000, 9);
